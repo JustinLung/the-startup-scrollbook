5510e1b4dfd0ef407c43f643c1a79f02fce0b32c
DaanKorver <daank2000@hotmail.nl>
2022-01-13 15:01:59 +0100

    svelte is no more

---
+++ nuxt.config.js
@@ -0,0 +1,50 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'Scrollbook - Interactive Reading',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    script: [
+      {src: "https://cdnjs.cloudflare.com/ajax/libs/gsap/3.9.1/gsap.min.js"},
+      {src: "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/ScrollMagic.min.js"},
+      {src: "https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/animation.gsap.js"},
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '@/styles/index.css'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,61 @@
+<script>
+  export default {
+    transition: {
+      name: 'fade',
+      mode: 'out-in'
+    }
+  }
+</script>
+
+<template>
+  <section>
+    <h1>Choose A Book To Preview</h1>
+    <figure>
+      <NuxtLink to="/lotr" class="link"><img src="~assets/Bookcover-thelordoftherings.png" alt="The Lord of the Rings Bookcover" /></NuxtLink>
+      <NuxtLink to="/the-shining" class="link"><img src="~assets/Bookcover-theshining.png" alt="The Shining Bookcover" /></NuxtLink>
+    </figure>
+    <img src="~assets/scrollbook-logo.svg" alt="">
+  </section>
+</template>
+
+<style scoped>
+section {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+h1 {
+  font-size: 2rem;
+  margin-bottom: 2em;
+}
+
+section > figure {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 4em;
+}
+
+img {
+  width: 15em;
+}
+
+figure .link {
+  transition: .3s ease;
+}
+
+figure .link:hover {
+  transform: scale(1.1);
+}
+
+section > img {
+  position: absolute;
+  bottom: 2em;
+  left: 2em;
+  width: 7em;
+}
+</style>
