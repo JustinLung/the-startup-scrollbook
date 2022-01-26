1df41547895e180bd5549ffc080e674e2723e60c
DaanKorver <DaanKorver@users.noreply.github.com>
2022-01-26 11:01:38 +0000

    deploy: 339774ed4fe1603836f157025891fc6120aea6d8

--- _nuxt/static/1643194795/the-shining/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1643194795",layout:"the-shining",error:a,serverRendered:true,routePath:"\u002Fthe-shining",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
